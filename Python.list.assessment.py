a1010faa0be55b4fba3e8384b41cdf163ca85638
Rinka <146360541+Rinka-Dokuta@users.noreply.github.com>
2024-02-09 09:44:52 -0700

    Create Python.list.assessment.py

---
+++ Python.list.assessment.py
@@ -0,0 +1,16 @@
+DiverseBeats = ["Tusa", "Rockstar", "Tangerine",]
+ 
+DiverseBeats.append("Happier")
+print(DiverseBeats)
+ 
+DiverseBeats.remove("Rockstar")
+print(DiverseBeats)
+  
+DiverseBeats.insert(2, 'Telepatia')
+print(DiverseBeats)
+    
+if "Watermelon Sugar" in  DiverseBeats:
+    print("True")
+else:
+    print("False")
+print( DiverseBeats)
